b04fdb0a0911905d604031611abc28fc41b54f48
Marius 't Hart <thartbm@gmail.com>
2025-09-26 10:24:45 -0400

    function to install Reach, function to download data

--- R/data.R
+++ R/data.R
@@ -2,3 +2,31 @@
 
 # get data from https://osf.io/kxmjs/
 
+getOSFdata <- function() {
+  
+  Reach::downloadOSFdata(
+    repository = 'kxmjs',
+    filelist   = list('data'='exp_2.zip', 'exp_1.zip'),
+    folder     = 'data',
+    overwrite  = TRUE,
+    unzip      = FALSE,
+    removezips = FALSE,
+    wait = 40
+  )
+  
+}
+
+
+installReach <- function() {
+  
+  # library('remotes')
+  ip <- installed.packages()
+  if ('Reach' %in% ip[,'Package']) {
+    if (ip[which(ip[,'Package'] == 'Reach'),'Version'] < "2023.12.17") {
+      remotes::install_github('thartbm/Reach')
+    }
+  } else {
+    remotes::install_github('thartbm/Reach')
+  }
+  
+}
